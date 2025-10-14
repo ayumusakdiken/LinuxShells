3f13411c05acd9f98bbcc57567e00dfb971e57e5
Kaano1 <ahmetyumusakdiken13@gmail.com>
2025-10-14 09:27:51 +0300

    Added: text-processing-in-linux-the-sed-command-1

---
+++ medium/text-processing-in-linux-the-sed-command-1/main.sh
@@ -0,0 +1,4 @@
+sed "s/\<the\>/this/"
+
+# \<the\>: Matches the word "the" only when it appears as a whole word, not as part of another word.
+# this: The replacement string that will replace occurrences of "the".
