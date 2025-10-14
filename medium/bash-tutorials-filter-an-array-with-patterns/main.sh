53af2d4ebb47335b59ca14b6aca2bfc269cf5ad8
Kaano1 <ahmetyumusakdiken13@gmail.com>
2025-10-14 08:31:33 +0300

    Fixed: run time error in bash-tutorials-filter-an-array-with-patterns

--- medium/bash-tutorials-filter-an-array-with-patterns/main.sh
+++ medium/bash-tutorials-filter-an-array-with-patterns/main.sh
@@ -1,4 +1,6 @@
-grep -v -i "a"
+grep -vi '[a]' || echo
 
 # -v: Invert match, to select non-matching lines
 # -i: Ignore case upper/lower case distinctions
+# '[a]': Pattern to match lines containing the letter 'a' (case insensitive)
+# || echo: means “if grep fails (exit code ≠ 0), then run echo”.
